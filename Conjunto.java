96c149c748f6fbfa73a24a9746cdf41d3a4a25f6
djgrego2 <gregorio_austin@hotmail.com>
2013-10-17 20:28:04 -0600

    Update Conjunto.java
    Static a Cada uno de los Metodos, para poder ser utilizados en la Clases ConjuntoTest

--- Conjunto.java
+++ Conjunto.java
@@ -11,15 +11,15 @@ import java.util.*;
  */
 public class Conjunto {
     
-    String elementos[] = new String[10];
-    public  Set<String> vacio = new TreeSet<>(Arrays.asList(elementos));
+    static String elementos[] = new String[10];
+    public  static Set<String> vacio = new TreeSet<>(Arrays.asList(elementos));
     
-    int dimension()
+    public static int dimension()
     {
         return elementos.length;
     }
     
-    int cardinal()
+    static int cardinal()
     {
         int Salida = 0, Contador;
         for(Contador = 0; Contador < dimension(); Contador++)
@@ -30,25 +30,25 @@ public class Conjunto {
         return Salida;
     }
     
-    Set union(Set Parametro) {
+    static Set union(Set Parametro) {
         Set Salida = new TreeSet(vacio);
         Salida.addAll(Parametro);
         return Salida;
     }
     
-    Set interseccion(Set Parametro) {
+    static Set interseccion(Set Parametro) {
         Set Salida = new TreeSet(vacio);
         Salida.retainAll(Parametro);
         return Salida;
     }
     
-    Set diferencia(Set Parametro) {
+    static Set diferencia(Set Parametro) {
         Set Salida = new TreeSet(vacio);
         Salida.removeAll(Parametro);
         return Salida;
     }
     
-    void desplegar()
+    static void desplegar()
     {
         System.out.println("Conjunto: " + vacio);
     }
