a1e2bcf53df804a6698e23d0dde492be82b0325c
djgrego2 <gregorio_austin@hotmail.com>
2013-10-21 12:40:53 -0600

    Update ConjuntoTest.java
    Cambio del formatos de las pruebas unitarias al JUnits 4.0

--- ConjuntoTest.java
+++ ConjuntoTest.java
@@ -2,61 +2,118 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ProyectoFinal;
-import java.util.*;
+package LABProyecto;
+
+import static LABProyecto.Conjunto.elementos;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author MINERD
  */
-public class ConjuntoTest{
+public class ConjuntoTest {
     
-    String elementos[] = {"Otra", "coleccion", "mas"};
-    public  Set<String> Prueba = new TreeSet<>(Arrays.asList(elementos));
-    
-    boolean dimensionTest()
-    {
-        int Salida = 4;
-        int Comparador = Conjunto.dimension();
-        if(Comparador == Salida)
-            return true;
-        else
-            return false;
+    public ConjuntoTest() {
     }
     
-    boolean cardinalTest()
-    {
-        int Salida = 4;
-        int Comparador = Conjunto.cardinal();
-        if(Comparador == Salida)
-            return true;
-        else
-            return false;
+    @BeforeClass
+    public static void setUpClass() {
     }
     
-    boolean unionTest() {
-        Set Resultado = Conjunto.union(Prueba);
-        if(Resultado.size() == 6)
-            return true;
-        else
-            return false;
+    @AfterClass
+    public static void tearDownClass() {
     }
     
-    boolean interseccionTest(Set Parametro) {
-        Set Resultado = Conjunto.union(Prueba);
-        if(Resultado.size() == 1)
-            return true;
-        else
-            return false;
+    @Before
+    public void setUp() {
     }
     
-    boolean diferenciaTest(Set Parametro) {
-        Set Resultado = Conjunto.union(Prueba);
-        if(Resultado.size() == 3)
-            return true;
-        else
-            return false;
+    @After
+    public void tearDown() {
     }
-    
-   
+
+    /**
+     * Test of dimension method, of class Conjunto.
+     */
+    @Test
+    public void testDimension() {
+        System.out.println("dimension");
+        int expResult = 4;
+        int result = Conjunto.dimension();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of cardinal method, of class Conjunto.
+     */
+    @Test
+    public void testCardinal() {
+        System.out.println("cardinal");
+        int expResult = 4;
+        int result = Conjunto.cardinal();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of union method, of class Conjunto.
+     */
+    @Test
+    public void testUnion() {
+        System.out.println("union");
+        String elementos[] = {"Esta", "es", "una", "coleccion"};
+        Set<String> Parametro = new TreeSet<>(Arrays.asList(elementos));
+        Set expResult = new TreeSet<>(Arrays.asList(elementos));
+        Set result = Conjunto.union(Parametro);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of interseccion method, of class Conjunto.
+     */
+    @Test
+    public void testInterseccion() {
+        System.out.println("interseccion");
+        String elementos[] = {"Esta", "es", "una", "coleccion"};
+        Set<String> Parametro = new TreeSet<>(Arrays.asList(elementos));
+        Set expResult = new TreeSet<>(Arrays.asList(elementos));
+        Set result = Conjunto.interseccion(Parametro);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of diferencia method, of class Conjunto.
+     */
+    @Test
+    public void testDiferencia() {
+        System.out.println("diferencia");
+        String elementos[] = {"Esta", "es", "una"};
+        String Resultado[] = {"coleccion"};
+        Set<String> Parametro = new TreeSet<>(Arrays.asList(elementos));
+        Set expResult = new TreeSet<>(Arrays.asList(Resultado));
+        Set result = Conjunto.diferencia(Parametro);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of desplegar method, of class Conjunto.
+     */
+    @Test
+    public void testDesplegar() {
+        System.out.println("desplegar");
+        Conjunto.desplegar();
+        // TODO review the generated test code and remove the default call to fail.
+    }
 }
